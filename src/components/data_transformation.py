0e5bae22ea5c6b4cee0c331d5acae36599517640
Ubuntu <ubuntu@ip-172-31-34-101.ec2.internal>
2026-05-18 07:04:28 +0000

    python files updated

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -88,10 +88,10 @@ class DataTransformation:
             target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            input_feature_train_df = train_df.drop(columns=[target_column_name])
             target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name])
             target_feature_test_df=test_df[target_column_name]
 
             logging.info(
